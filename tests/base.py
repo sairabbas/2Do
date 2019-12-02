05e0494d5020e4c6de98ee6332951adcd1d6dfcc
AnhMinhHoangLe <leanh6998@gmail.com>
2019-12-02 15:57:46 -0800

    update txt

--- tests/base.py
+++ tests/base.py
@@ -1,13 +1,10 @@
 from flask_testing import TestCase
-from flask import Flask
+
 from app import app, db
 from app.models import User, newList
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
+        app.config.from_object("config")
         return app
-        # app.config.from_object("config")
-        # return app
